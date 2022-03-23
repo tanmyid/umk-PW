cf1fcc0f570c0280950cc33c0f3590f48e55990d
tanmyid <tanmyid1337@gmail.com>
2022-03-23 21:20:01 +0700

    minggu-8

---
+++ minggu-8/nilai_predikat.js
@@ -0,0 +1,13 @@
+// percabangan if else if
+var nilai = prompt("Masukkan IPK , 2.00 - 4.00");
+var ket;
+if (nilai >= 2.00 && nilai <= 2.75) {
+	ket = "Memuaskan";
+} else if (nilai >= 2.76 && nilai <= 3.50) {
+	ket = "Sangat Memuaskan";
+} else if (nilai >= 3.51 && nilai <= 4.00) {
+	ket = "Mantap Lur";
+} else {
+	ket = "Angka yang dimasukkan tidak sesuai";
+}
+document.write(ket);
